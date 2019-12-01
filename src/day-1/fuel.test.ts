bb8b0b02451d2bef6501976159156fc5badd4744
Michael Lundin <mikel@ifit.com>
2019-12-01 09:45:01 -0800

    test: add initial unit tests for part 2

--- src/day-1/fuel.test.ts
+++ src/day-1/fuel.test.ts
@@ -1,10 +1,18 @@
-import { calculateFuelCost } from "./fuel";
+import { calculateFuelCost, calculateRecursiveFuelCost } from "./fuel";
 
 describe("fuel", () => {
   describe("calculateFuelCost", () => {
     it("returns correct results based on examples", () => {
       expect(calculateFuelCost(1969)).toEqual(654);
       expect(calculateFuelCost(100756)).toEqual(33583);
+    });
+  });
+
+  describe("calculateRecursiveFuelCost", () => {
+    it("returns correct results based on examples", () => {
+      expect(calculateRecursiveFuelCost(14)).toEqual(2);
+      expect(calculateRecursiveFuelCost(1969)).toEqual(654);
+      expect(calculateRecursiveFuelCost(100756)).toEqual(33583);
     });
   });
 });
--- src/day-1/fuel.ts
+++ src/day-1/fuel.ts
@@ -1,3 +1,7 @@
 export function calculateFuelCost(mass: number): number {
   return Math.floor(mass / 3) - 2;
 }
+
+export function calculateRecursiveFuelCost(mass: number): number {
+  return 0;
+}
