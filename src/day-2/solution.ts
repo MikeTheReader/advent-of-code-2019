a78126ea2163997407581482d1b550f32a1a244b
Michael Lundin <mikel@ifit.com>
2019-12-02 07:46:38 -0800

    feat: Day 2 - solution implementation
    Gold star earned. :)

--- src/day-2/solution.ts
+++ src/day-2/solution.ts
@@ -1,3 +1,17 @@
 import Solution from "../solution-base";
+import FileReader from "../utils/file-reader";
+import { runProgram } from "./intcode";
 
-export default class DayTwoSolution extends Solution {}
+export default class DayTwoSolution extends Solution {
+  public async executeFirstHalf() {
+    let program: number[];
+    const reader = new FileReader(line => {
+      program = line.split(",").map(x => +x);
+    });
+    await reader.processFile(this.file);
+    program[1] = 12;
+    program[2] = 2;
+    const results = runProgram(program);
+    return results[0];
+  }
+}
