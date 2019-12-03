e2d20b941e05281f75b4e07cdcba4564d8e3f16f
Michael Lundin <mikel@ifit.com>
2019-12-03 13:48:00 -0800

    feat: Day 3 - first half solution

--- src/day-3/solution.ts
+++ src/day-3/solution.ts
@@ -1,3 +1,11 @@
 import Solution from "../solution-base";
+import { processFile } from "../utils/file-reader";
+import { Grid } from "./wires";
 
-export default class DayThreeSolution extends Solution {}
+export default class DayThreeSolution extends Solution {
+  public async executeFirstHalf() {
+    const grid = new Grid();
+    await processFile(this.file, line => grid.addWire(line));
+    return grid.getSmallestDistance();
+  }
+}
