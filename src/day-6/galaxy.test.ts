ad88d6c9b228aad1d8235e870d0fec95f8582665
Michael Lundin <mikel@ifit.com>
2019-12-06 07:16:56 -0800

    feat: Day 6 - implement addOrbit

--- src/day-6/galaxy.test.ts
+++ src/day-6/galaxy.test.ts
@@ -3,13 +3,13 @@ import { Galaxy } from './galaxy';
 describe('galaxy', () => {
   describe('addOrbit', () => {
     it('adds an orbit link', () => {
+      const expectedRoot = { name: 'COM', orbits: [], orbiters: [] };
+      const expectedOrbiter = { name: 'B', orbits: [expectedRoot], orbiters: [] };
+      expectedRoot.orbiters = [expectedOrbiter];
+
       const galaxy = new Galaxy();
       galaxy.addOrbit('COM)B');
-      expect(galaxy.getRoot()).toEqual({
-        name: 'COM',
-        orbiters: [{ name: 'B' }],
-        orbits: []
-      });
+      expect(galaxy.getRoot()).toEqual(expectedRoot);
     });
   });
 });
--- src/day-6/galaxy.ts
+++ src/day-6/galaxy.ts
@@ -5,15 +5,25 @@ export interface Planet {
 }
 
 export class Galaxy {
+  private planets = {};
+
   constructor() {
     // to implement
   }
 
   public getRoot(): Planet {
-    return null;
+    // tslint:disable-next-line: no-string-literal
+    return this.planets['COM'];
   }
 
   public addOrbit(planetStr: string): void {
-    // to implement
+    const orbitRegex = /(.*)\)(.*)/;
+    const matches = planetStr.match(orbitRegex);
+    const name = matches[1];
+    const orbiter = matches[2];
+    this.planets[name] = this.planets[name] || { name, orbiters: [], orbits: [] };
+    this.planets[orbiter] = this.planets[orbiter] || { name: orbiter, orbiters: [], orbits: [] };
+    this.planets[name].orbiters.push(this.planets[orbiter]);
+    this.planets[orbiter].orbits.push(this.planets[name]);
   }
 }
