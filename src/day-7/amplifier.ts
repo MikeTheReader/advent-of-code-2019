34f092eb29a996d5706adc4d99f6a116760011c3
Michael Lundin <mikel@ifit.com>
2019-12-07 07:32:11 -0800

    feat: Day 7 - implement getAllPermutations

--- src/day-7/amplifier.ts
+++ src/day-7/amplifier.ts
@@ -1,3 +1,23 @@
-export function getAllPermutations(digits: number[]): number[][] {
-  return [];
+export function getAllPermutations(original: number[]): number[][] {
+  const permutations = [];
+  function permute(digits: number[], size: number, n: number) {
+    if (size === 1) {
+      permutations.push(digits.slice());
+    }
+    for (let i = 0; i < size; i++) {
+      permute(digits, size - 1, n);
+
+      if (size % 2 === 1) {
+        const buffer = digits[0];
+        digits[0] = digits[size - 1];
+        digits[size - 1] = buffer;
+      } else {
+        const buffer = digits[i];
+        digits[i] = digits[size - 1];
+        digits[size - 1] = buffer;
+      }
+    }
+  }
+  permute(original, original.length, original.length);
+  return permutations;
 }
