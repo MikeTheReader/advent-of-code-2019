70414530d0ceb53ffab565096d3c0b9c44db6b47
Michael Lundin <mikel@ifit.com>
2019-12-07 07:56:51 -0800

    feat: Day 7 - first half solution

--- src/day-7/solution.ts
+++ src/day-7/solution.ts
@@ -1,3 +1,13 @@
 import Solution from '../solution-base';
+import { processFile } from '../utils/file-reader';
+import { amplify } from './amplifier';
 
-export default class DaySevenSolution extends Solution {}
+export default class DaySevenSolution extends Solution {
+  public async executeFirstHalf() {
+    let program;
+    await processFile(this.file, line => {
+      program = line.split(',').map(x => +x);
+    });
+    return amplify(program);
+  }
+}
