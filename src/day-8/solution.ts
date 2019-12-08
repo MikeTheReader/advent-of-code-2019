22e7b371ab3e2ddc60b97c99beac7bea2578c239
Michael Lundin <mikel@ifit.com>
2019-12-08 07:58:36 -0800

    feat: Day 8 - first half solution

--- src/day-8/solution.ts
+++ src/day-8/solution.ts
@@ -1,3 +1,13 @@
 import Solution from '../solution-base';
+import { processFile } from '../utils/file-reader';
+import { breakUpLayers, checksum, countOccurences } from './image';
 
-export default class DayEightSolution extends Solution {}
+export default class DayEightSolution extends Solution {
+  public async executeFirstHalf() {
+    let image: string;
+    await processFile(this.file, line => (image = line));
+    const imageLayers = breakUpLayers({ height: 6, width: 25 }, image);
+    const layerCounts = imageLayers.map(layer => countOccurences(layer));
+    return checksum(layerCounts);
+  }
+}
