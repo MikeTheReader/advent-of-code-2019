32b06a27172e410cdb6fd9dc25c7543ce09cdc47
Michael Lundin <mikel@ifit.com>
2019-12-09 14:44:57 -0800

    feat: Day 9 - second half solution

--- src/day-9/solution.ts
+++ src/day-9/solution.ts
@@ -3,11 +3,21 @@ import { processFile } from '../utils/file-reader';
 import { runProgram } from './intcodeV9';
 
 export default class DayNineSolution extends Solution {
-  public async executeFirstHalf() {
+  public async initProgram(): Promise<number[]> {
     let program: number[];
     await processFile(this.file, line => {
       program = line.split(',').map(x => +x);
     });
-    return runProgram(program, [1]).output;
+    return program;
+  }
+
+  public async executeFirstHalf() {
+    const program = await this.initProgram();
+    return runProgram(program, [1]).output[0];
+  }
+
+  public async executeSecondHalf() {
+    const program = await this.initProgram();
+    return runProgram(program, [2]).output[0];
   }
 }
