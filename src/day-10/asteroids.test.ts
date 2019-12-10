7be32926c4bfa4e9a39992edf0c62eb3cb3caccb
Michael Lundin <mikel@ifit.com>
2019-12-10 10:09:10 -0800

    feat: Day 10 - implemented AsteroidMap class
    Modified from ingestMap since the class allowed an easier
    getter method to return coordinates in the usual x,y format.

--- src/day-10/asteroids.test.ts
+++ src/day-10/asteroids.test.ts
@@ -1,46 +1,51 @@
-import { ingestMap } from './asteroids';
+import { AsteroidMap } from './asteroids';
 
 describe('asteroids', () => {
-  describe('ingestMap', () => {
-    it('ingests the test maps correctly', () => {
-      const mapOneStr =
-        '......#.#.\n' +
-        '#..#.#....\n' +
-        '..#######.\n' +
-        '.#.#.###..\n' +
-        '.#..#.....\n' +
-        '..#....#.#\n' +
-        '#..#....#.\n' +
-        '.##.#..###\n' +
-        '##...#..#.\n' +
-        '.#....####';
-      expect(ingestMap(mapOneStr)[5][8]).toBe('#');
+  describe('AsteroidMap', () => {
+    describe('constructor', () => {
+      it('ingests the test maps correctly', () => {
+        const mapOneStr =
+          '......#.#.\n' +
+          '#..#.#....\n' +
+          '..#######.\n' +
+          '.#.#.###..\n' +
+          '.#..#.....\n' +
+          '..#....#.#\n' +
+          '#..#....#.\n' +
+          '.##.#..###\n' +
+          '##...#..#.\n' +
+          '.#....####';
+        const mapOne = new AsteroidMap(mapOneStr);
+        expect(mapOne.getCoordinate(5, 8)).toBe('#');
 
-      const mapTwoStr =
-        '#.#...#.#.\n' +
-        '.###....#.\n' +
-        '.#....#...\n' +
-        '##.#.#.#.#\n' +
-        '....#.#.#.\n' +
-        '.##..###.#\n' +
-        '..#...##..\n' +
-        '..##....##\n' +
-        '......#...\n' +
-        '.####.###.';
-      expect(ingestMap(mapTwoStr)[1][2]).toBe('#');
+        const mapTwoStr =
+          '#.#...#.#.\n' +
+          '.###....#.\n' +
+          '.#....#...\n' +
+          '##.#.#.#.#\n' +
+          '....#.#.#.\n' +
+          '.##..###.#\n' +
+          '..#...##..\n' +
+          '..##....##\n' +
+          '......#...\n' +
+          '.####.###.';
+        const mapTwo = new AsteroidMap(mapTwoStr);
+        expect(mapTwo.getCoordinate(1, 2)).toBe('#');
 
-      const mapThreeStr =
-        '.#..#..###\n' +
-        '####.###.#\n' +
-        '....###.#.\n' +
-        '..###.##.#\n' +
-        '##.##.#.#.\n' +
-        '....###..#\n' +
-        '..#.#..#.#\n' +
-        '#..#.#.###\n' +
-        '.##...##.#\n' +
-        '.....#.#..';
-      expect(ingestMap(mapThreeStr)[6][3]).toBe('#');
+        const mapThreeStr =
+          '.#..#..###\n' +
+          '####.###.#\n' +
+          '....###.#.\n' +
+          '..###.##.#\n' +
+          '##.##.#.#.\n' +
+          '....###..#\n' +
+          '..#.#..#.#\n' +
+          '#..#.#.###\n' +
+          '.##...##.#\n' +
+          '.....#.#..';
+        const mapThree = new AsteroidMap(mapThreeStr);
+        expect(mapThree.getCoordinate(6, 3)).toBe('#');
+      });
     });
   });
 });
--- src/day-10/asteroids.ts
+++ src/day-10/asteroids.ts
@@ -1,3 +1,19 @@
-export function ingestMap(mapString: string): string[][] {
-  return [['#']];
+export class AsteroidMap {
+  private grid: string[][];
+
+  constructor(mapString: string) {
+    this.grid = [];
+    const rows = mapString.split('\n');
+    rows.forEach((row, y) => {
+      this.grid[y] = [];
+      const columns = row.split('');
+      columns.forEach((column, x) => {
+        this.grid[y][x] = column;
+      });
+    });
+  }
+
+  public getCoordinate(x: number, y: number): string {
+    return this.grid[y][x];
+  }
 }
