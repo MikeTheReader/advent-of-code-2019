9f35b14921983c6b1cb78c36d83ef6b67cbd4c3e
Michael Lundin <mikel@ifit.com>
2019-12-30 08:49:41 -0800

    feat: Day 14 - first half solution

--- src/Day-14/solution.ts
+++ src/Day-14/solution.ts
@@ -1,3 +1,11 @@
 import Solution from '../solution-base';
+import { processFile } from '../utils/file-reader';
+import { calculateOre } from './reactions';
 
-export default class DayFourteenSolution extends Solution {}
+export default class DayFourteenSolution extends Solution {
+  public async executeFirstHalf() {
+    const reactionStrings: string[] = [];
+    await processFile(this.file, line => reactionStrings.push(line));
+    return calculateOre(reactionStrings);
+  }
+}
