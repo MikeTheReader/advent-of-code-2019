0a9b202ee00c389b55984d38505633f599c3a508
Michael Lundin <mikel@ifit.com>
2020-06-14 20:12:52 -0700

    refactor: remove unused grid
    Final solution did not use grid, so removed it, as well as other
    functions that were note used. It's possible those will return in
    part II, so it's nice that this is all in a respoitory.

--- src/day-15/repair.ts
+++ src/day-15/repair.ts
@@ -1,5 +1,4 @@
 import { IntCode } from '../day-11/intcodeV11';
-import Grid, { Coordinate } from '../utils/grid';
 
 enum MovementCommand {
   North = 1,
@@ -13,21 +12,6 @@ enum StatusCodes {
   MovedOneStep = 1,
   FoundOxygenSystem = 2
 }
-/*
-const toTheRight = {
-  [MovementCommand.North]: MovementCommand.East,
-  [MovementCommand.East]: MovementCommand.South,
-  [MovementCommand.South]: MovementCommand.West,
-  [MovementCommand.West]: MovementCommand.North
-};
-
-const toTheLeft = {
-  [MovementCommand.North]: MovementCommand.West,
-  [MovementCommand.West]: MovementCommand.South,
-  [MovementCommand.South]: MovementCommand.East,
-  [MovementCommand.East]: MovementCommand.North
-};
-*/
 
 const oppositeDirection = {
   [MovementCommand.North]: MovementCommand.South,
@@ -36,66 +20,12 @@ const oppositeDirection = {
   [MovementCommand.East]: MovementCommand.West
 };
 
-function getNewLocation(gridLocation: Coordinate, direction: MovementCommand): Coordinate {
-  if (direction === MovementCommand.North) {
-    return { x: gridLocation.x, y: gridLocation.y - 1 };
-  } else if (direction === MovementCommand.South) {
-    return { x: gridLocation.x, y: gridLocation.y + 1 };
-  } else if (direction === MovementCommand.East) {
-    return { x: gridLocation.x + 1, y: gridLocation.y };
-  } else if (direction === MovementCommand.West) {
-    return { x: gridLocation.x - 1, y: gridLocation.y };
-  }
-}
-
 export function findOxygenSteps(processor: IntCode): number {
-  const gridSpan = 46;
-  const grid = new Grid();
-  grid.fill(' ', { height: gridSpan, width: gridSpan });
-  const gridLocation = { x: gridSpan / 2, y: gridSpan / 2 };
-
-  const smallest = recursiveFind([], gridLocation, MovementCommand.North, processor);
+  const smallest = recursiveFind([], MovementCommand.North, processor);
   return smallest;
-  /*const directionsMoved: number[] = [];
-  let currentDirection = MovementCommand.North;
-  let iteration = 0;
-  directionsMoved.push(currentDirection);
-  grid.setValue(gridLocation, 'S');
-  while (iteration < 5000) {
-    if (gridLocation.x === startingLocation.x && gridLocation.y === startingLocation.y) {
-      // Don't mess with the starting location's value
-    } else if (grid.getValue(gridLocation) !== '#') {
-      grid.setValue(gridLocation, 'O');
-      // tslint:disable
-      console.clear();
-      console.log(grid.toString());
-      // tslint:enable
-      grid.setValue(gridLocation, ' ');
-    }
-    const output = processor.runProgram(directionsMoved).output[iteration];
-    if (output === StatusCodes.HitAWall) {
-      grid.setValue(getNewLocation(gridLocation, currentDirection), 'X');
-      currentDirection = toTheRight[currentDirection];
-    } else if (output === StatusCodes.MovedOneStep) {
-      gridLocation = getNewLocation(gridLocation, currentDirection);
-      const previousCommands = directionsMoved.slice();
-      previousCommands.push(toTheLeft[currentDirection]);
-      const checkRight = processor.runProgram(directionsMoved).output[iteration + 1];
-      if (checkRight !== StatusCodes.HitAWall) {
-        currentDirection = toTheLeft[currentDirection];
-      }
-    } else if (output === StatusCodes.FoundOxygenSystem) {
-      gridLocation = getNewLocation(gridLocation, currentDirection);
-      grid.setValue(gridLocation, '#');
-    }
-    directionsMoved.push(currentDirection);
-    iteration++;
-  }
-
-  return iteration;*/
 }
 
-function recursiveFind(previousCommands, gridLocation, currentDirection, processor) {
+function recursiveFind(previousCommands, currentDirection, processor) {
   const allExits: MovementCommand[] = findAllExits(previousCommands, processor);
   const viableExits = allExits.filter(direction => direction !== oppositeDirection[currentDirection]);
   if (!viableExits.length) {
@@ -116,8 +46,7 @@ function recursiveFind(previousCommands, gridLocation, currentDirection, process
   const numStepsToGoal = viableExits.map(direction => {
     const newCommands = previousCommands.slice();
     newCommands.push(direction);
-    const newGridLocation = getNewLocation(gridLocation, direction);
-    return recursiveFind(newCommands, newGridLocation, direction, processor);
+    return recursiveFind(newCommands, direction, processor);
   });
 
   const positiveSteps = numStepsToGoal.filter(steps => steps > -1);
